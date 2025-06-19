5d9c04015a32efdd39ae0412a93e7965d5750e31
Siddhant Rai <105770390+Siddhant20020@users.noreply.github.com>
2025-06-19 09:12:43 +0545

    Completed

--- src/containers/skills/utils.js
+++ src/containers/skills/utils.js
@@ -11,7 +11,7 @@ export const skillsData = [
     ],
   },
   {
-    label: "Frontend Development",
+    label: "Frontend",
     data: [
       { skillName: "React.js", percentage: 90 },
       { skillName: "JavaScript", percentage: 85 },
@@ -20,7 +20,7 @@ export const skillsData = [
     ],
   },
   {
-    label: "Backend Development",
+    label: "Backend",
     data: [
       { skillName: "Node.js", percentage: 75 },
       { skillName: "Express.js", percentage: 75 },
